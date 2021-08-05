b551460f8a0981c9fb5a17fc645747d92e7fccc1
KnockOutEZ <ktowhid20@gmail.com>
2021-08-05 10:49:23 +0600

    try1

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,14 +3,16 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+     <router-link to="/">
+        <a class="text-white font-bold uppercase text-2xl mr-4">Techynaf</a>
+     </router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
           <li>
-            <a class="px-2 text-white" @click="toggleAuthModal" href="#">
+            <a class="px-2 text-white" @click="toggleAuthModal">
               Login / Register
               </a>
           </li>
